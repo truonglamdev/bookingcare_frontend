adff82d0a49284734413974544b4a660c80fb1f9
truonglamdev <truonglamdev070303@gmail.com>
2022-07-16 08:08:31 +0700

    handle event form login

--- src/containers/Auth/Login.js
+++ src/containers/Auth/Login.js
@@ -1,7 +1,7 @@
+/* eslint-disable no-undef */
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { push } from 'connected-react-router';
-import {  } from 'bootstrap';
 
 import * as actions from '../../store/actions';
 
@@ -12,7 +12,34 @@ class Login extends Component {
     constructor(props) {
         super(props);
         this.btnLogin = React.createRef();
+        this.state = {
+            username: '',
+            password: '',
+            showPassword: false,
+        };
     }
+
+    handleOnChangeUsername = (e) => {
+        this.setState({
+            username: e.target.value,
+        });
+    };
+
+    handleOnChangePassword = (e) => {
+        this.setState({
+            password: e.target.value,
+        });
+    };
+
+    handleOnClickSubmit = () => {
+        alert('submit successfully');
+    };
+
+    handleShowHidePassword = () => {
+        this.setState({
+            showPassword: !this.state.showPassword,
+        });
+    };
 
     render() {
         return (
@@ -22,14 +49,38 @@ class Login extends Component {
                         <div className="col-12 text-login">Login</div>
                         <div className="col-12 form-group login-input">
                             <label>User name:</label>
-                            <input type="text" className="form-control" placeholder="Enter your username" />
+                            <input
+                                type="text"
+                                value={this.state.username}
+                                onChange={(e) => this.handleOnChangeUsername(e)}
+                                className="form-control"
+                                placeholder="Enter your username"
+                            />
                         </div>
                         <div className="col-12 form-group login-input">
                             <label>Password:</label>
-                            <input type="password" className="form-control" placeholder="Enter your password" />
+                            <div className="custom-input-password">
+                                <input
+                                    type={this.state.showPassword ? 'text' : 'password'}
+                                    value={this.state.password}
+                                    onChange={(e) => this.handleOnChangePassword(e)}
+                                    className="form-control password"
+                                    placeholder="Enter your password"
+                                />
+                                <span onClick={this.handleShowHidePassword} className="eye-hide-password">
+                                    {this.state.showPassword ? (
+                                        <i className="far fa-eye"></i>
+                                    ) : (
+                                        <i className="far fa-eye-slash"></i>
+                                    )}
+                                </span>
+                            </div>
                         </div>
+
                         <div className="col-12 form-group login-btn">
-                            <button type="submit">Login</button>
+                            <button type="submit" onClick={this.handleOnClickSubmit}>
+                                Login
+                            </button>
                         </div>
                         <div className="col-12 forgot-password">
                             <span className="">Forgot your password?</span>
@@ -37,8 +88,9 @@ class Login extends Component {
                         <div className="col-12 login-with">
                             <span>Or login with:</span>
                         </div>
-                        <div className="col-12">
-                            
+                        <div className="col-12 social-login">
+                            <i className="fab fa-google-plus-g google-icon "></i>
+                            <i className="fab fa-facebook-f facebook-icon"></i>
                         </div>
                     </div>
                 </div>
